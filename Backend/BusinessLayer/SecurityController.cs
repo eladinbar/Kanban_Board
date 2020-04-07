7ab3fb8ab519f139c8f61370df57a4f10b95c704
paveltom <paveltom@post.bgu.ac.il>
2020-04-07 17:11:33 +0300

    routine commit: methods added

--- Backend/BusinessLayer/SecurityController.cs
+++ Backend/BusinessLayer/SecurityController.cs
@@ -8,5 +8,37 @@ namespace IntroSE.Kanban.Backend.BusinessLayer
 {
     class SecurityController
     {
+        private UserPackage.UserController UserControl;
+        private BoardPackage.BoardController BoardControl;
+        private UserPackage.User CurrentUse;
+
+        public SecurityController()
+        {
+            UserControl = new UserPackage.UserController();
+            BoardControl = new BoardPackage.BoardController();
+            CurrentUser = null;
+        }
+
+        public UserPackage.UserController UserController 
+        {
+            get { return UserControl; }
+        }
+
+        public BoardPackage.BoardController BoardController
+        {
+            get { return BoardControl; }
+        }
+
+        public UserPackage.User CurrentUser
+        {
+            get { return CurrentUse; }
+            set { CurrentUse = value; }
+
+        }
+
+        public bool UserValidation (string email)
+        {
+            return false; // change to: return CurrentUse.Email().Equals(email); after class User is created in BL.
+        }
     }
 }
