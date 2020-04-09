332a23580ad1e0713fc865a7e7c25dbafad20853
Edan Levy <edal@post.bgu.ac.il>
2020-04-09 18:26:13 +0300

    Board controller empty methods

--- Backend/BusinessLayer/BoardPackage/Column.cs
+++ Backend/BusinessLayer/BoardPackage/Column.cs
@@ -8,5 +8,30 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
 {
     public class Column : PresistedObject<DataAccessLayer.Column>
     {
+        private string _name;
+        private int _limit;
+        private List<Task> Tasks;
+
+        public Column (string name)
+        {
+            
+        }
+
+        public void LimitColumnTasks(int limit)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertTask(Task t)
+        {
+            throw new NotImplementedException();
+        }
+         public Task RemoveTask(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
     }
 }
