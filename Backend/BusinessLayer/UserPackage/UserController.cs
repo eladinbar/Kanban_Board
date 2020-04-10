b6163ca15c285788d56bd3d2e66792a2b7d829e3
eladinbar <eladin@post.bgu.ac.il>
2020-04-10 11:10:05 +0300

    Login return User + ValidatePassword comments

--- Backend/BusinessLayer/UserPackage/UserController.cs
+++ Backend/BusinessLayer/UserPackage/UserController.cs
@@ -17,11 +17,6 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.UserPackage
             this.Users = dalC.LoadAllUsers();
         }
 
-        internal Dictionary<string, User> users
-        {
-            get { return Users; }
-        }
-
         public void Register(string email, string password, string nickname) {
             if (!Users.ContainsKey(email)) {
                User newUser = new User(email, password, nickname);
@@ -31,13 +26,14 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.UserPackage
                throw new ArgumentException("A user with this E-mail address already exists, please re-evaluate your information and try again.");
         }
 
-        public void Login (string email, string password) {
+        public User Login (string email, string password) {
             if (!Users.ContainsKey(email))
                 throw new ArgumentException("The E-mail given does not exist in the database, please register and try again.");
             else if (!Users[email].password.Equals(password))
                 throw new ArgumentException("Incorrect password. Please try again.");
             else
                 Users[email].Login();
+            return Users[email];
         }
 
         public void Logout (string email) {
@@ -54,7 +50,9 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.UserPackage
         }
 
         private void ValidatePassword (string password) {
-            if (password.Length < 4 | password.Length > 20 | !Regex.IsMatch(password, "^[a-zA-Z0-9]*$") | !password.Any(char.IsDigit) | !password.Any(char.IsLower) | !password.Any(char.IsUpper))
+            if (password.Length < 4 | password.Length > 20 || //Checks that the password length is within the appropriate bounds
+            !Regex.IsMatch(password, "^[a-zA-Z0-9]*$") | //Checks that the password consists of alphanumerical characters only
+            !password.Any(char.IsDigit) | !password.Any(char.IsLower) | !password.Any(char.IsUpper)) //Checks if the password contains at least 1 digit, lowercase letter and uppercase letter
                 throw new ArgumentException("A user password must be in length of 4 to 20 characters and must include at least one uppercase letter, one lowercase letter and a number.");
         }
     }
