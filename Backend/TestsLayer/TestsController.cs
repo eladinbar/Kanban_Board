b0da6ad3d9755610cd5c78f14526248814978e63
paveltom <paveltom@post.bgu.ac.il>
2020-04-13 10:19:39 +0300

    random users and tasks creator added

--- Backend/TestsLayer/TestsController.cs
+++ Backend/TestsLayer/TestsController.cs
@@ -15,24 +15,19 @@ namespace IntroSE.Kanban.Backend.TestsLayer
             Stopwatch timer = new Stopwatch();
             timer.Start();
             Console.WriteLine(Path.GetFullPath(@"..\..\") + "data\\");
-            //declaring propper users and their passwords
-            ServiceLayer.User newProperUser1 = new ServiceLayer.User("propUser1@mashu.com", "properNick1");
-            string propUser1Password = "abC123";
+            
+            //declaring random users
+            UserForTestCreator userForTestCreator = new UserForTestCreator(5);
+            List<ServiceLayer.User> randomUsers = userForTestCreator._users;
+            userForTestCreator.PrintUsers();
 
-            ServiceLayer.User newProperUser2 = new ServiceLayer.User("propUser2@mashu.com", "properNick2");
-            string propUser2Password = "abC123";
+            string uniPassword = "abC123";
 
-            ServiceLayer.User newProperUser3 = new ServiceLayer.User("propUser3@mashu.com", "properNick3");
-            string propUser3Password = "abC123";
+            //declaring random tasks
+            TaskForTestCreator taskForTestCreator = new TaskForTestCreator(5);
+            List<ServiceLayer.Task> randomTasks = taskForTestCreator._tasks;
+            taskForTestCreator.PrintTasks();
 
-
-            //declaring propper tasks
-            DateTime dueDate = new DateTime(26 / 03 / 2035);
-            ServiceLayer.Task properTask1 = new ServiceLayer.Task(123, DateTime.Now, "firstTask", "lorem ipsum1", dueDate);
-
-            ServiceLayer.Task properTask2 = new ServiceLayer.Task(123, DateTime.Now, "secondTask", "lorem ipsum2", dueDate);
-
-            ServiceLayer.Task properTask3 = new ServiceLayer.Task(123, DateTime.Now, "thirdTask", "lorem ipsum3", dueDate);
 
             //declaring new Service.cs appearance.
             ServiceLayer.Service service = new ServiceLayer.Service();
@@ -44,11 +39,21 @@ namespace IntroSE.Kanban.Backend.TestsLayer
             ldTest.LoadData(service);
             ldTest.LoadData(service);
 
+            //Register tests - random users registration
             RegisterTest regTest = new RegisterTest(service);
-            regTest.AllGood(newProperUser1, propUser1Password);
+            foreach (ServiceLayer.User tempUser in randomUsers)
+                regTest.AllGood(tempUser, uniPassword);
 
+            
+            //Login Test - first login
             LoginTest loginTest = new LoginTest(service);
-            loginTest.AllGood(newProperUser1, propUser1Password);
+            loginTest.AllGood(randomUsers.ElementAt(0), uniPassword);
+
+
+
+
+
+
 
 
 
