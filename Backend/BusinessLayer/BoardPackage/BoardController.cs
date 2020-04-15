b131098333ad50c8b9bcf9a89af99e6064b2daa8
Edan Levy <edal@post.bgu.ac.il>
2020-04-15 12:56:37 +0300

    Board controller docsumentation

--- Backend/BusinessLayer/BoardPackage/BoardController.cs
+++ Backend/BusinessLayer/BoardPackage/BoardController.cs
@@ -8,6 +8,10 @@ using System.Threading.Tasks;
 
 namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
 {
+    /// <summary>
+    /// The BoardController is the class that controls the functionality of the BoardPackage.
+    /// Contains the methods for adding and modifying the content of a board.
+    /// </summary>
     public class BoardController
     {
         private static readonly log4net.ILog log = LogHelper.getLogger();
@@ -31,7 +35,17 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
                 _boards.Add(DALboard.UserEmail, new Board(DALboard.UserEmail, DALboard.TaskCounter, columns));
             }
         }
-              
+         
+        /// <summary>
+        /// Gets the Board woth the key equals to email.
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong</param>
+        /// <returns> 
+        /// The Board of the user with that email
+        /// </returns>
+        /// <exception cref="ArgumentException.ArgumentException(string)">
+        /// Thrown when there is no key that equals to email in Boards dictionary 
+        /// </exception>
         public Board GetBoard(string email)
         {
             Board tempBoard;
@@ -40,19 +54,36 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             else
                 throw new ArgumentException("board not exist with this email");
         }
-
+        /// <summary>
+        /// Get the column in the board with the email and the specified columnName 
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong</param>
+        /// <param name="columnName">The name of the column in the board </param>
+        /// <returns>return Column with the spesified name in the board with the key email</returns>
         public Column GetColumn(string email, string columnName)
         {
             Board newBoard = GetBoard(email);
             return newBoard.GetColumn(columnName);
         }
-
+        /// <summary>
+        /// Get the column in the board with the key email and the specified columnOrdinal 
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong</param>
+        /// <param name="columnOrdinal">the column number of the board</param>
+        /// <returns>return Column with the spesified columnOrdinal in the board with the key email</returns>
         public Column GetColumn(string email, int columnOrdinal)
         {
             Board b = GetBoard(email);
             return b.GetColumn(columnOrdinal);
         }
 
+        /// <summary>
+        /// limit the number of taks that the columns with columnOrdinal can hold
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong</param>
+        /// <param name="columnOrdinal">the column number of the board</param>
+        /// <param name="limit">The number of maximum task that the column will hold</param>
+        /// <exception cref="ArgumentException">theown when tring to set limit to s number less or equal to 0</exception>
         public void LimitColumnTask(string email, int columnOrdinal, int limit)
         {
             Board b = GetBoard(email);
