84b4834c21e1c9ed5478f7e7edeb02d15164ba7c
Edan Levy <edal@post.bgu.ac.il>
2020-04-15 13:15:57 +0300

    BoardController Documentation

--- Backend/BusinessLayer/BoardPackage/BoardController.cs
+++ Backend/BusinessLayer/BoardPackage/BoardController.cs
@@ -39,12 +39,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
         /// <summary>
         /// Gets the Board woth the key equals to email.
         /// </summary>
-        /// <param name="email">the email of the user that the board belong</param>
+        /// <param name="email">the email of the user that the board belong.</param>
         /// <returns> 
-        /// The Board of the user with that email
+        /// The Board of the user with that email.
         /// </returns>
         /// <exception cref="ArgumentException.ArgumentException(string)">
-        /// Thrown when there is no key that equals to email in Boards dictionary 
+        /// Thrown when there is no key that equals to email in Boards dictionary.
         /// </exception>
         public Board GetBoard(string email)
         {
@@ -55,22 +55,22 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
                 throw new ArgumentException("board not exist with this email");
         }
         /// <summary>
-        /// Get the column in the board with the email and the specified columnName 
+        /// Get the column in the board with the email and the specified columnName.
         /// </summary>
-        /// <param name="email">the email of the user that the board belong</param>
-        /// <param name="columnName">The name of the column in the board </param>
-        /// <returns>return Column with the spesified name in the board with the key email</returns>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnName">The name of the column in the board.</param>
+        /// <returns>return Column with the spesified name in the board with the key email.</returns>
         public Column GetColumn(string email, string columnName)
         {
             Board newBoard = GetBoard(email);
             return newBoard.GetColumn(columnName);
         }
         /// <summary>
-        /// Get the column in the board with the key email and the specified columnOrdinal 
+        /// Get the column in the board with the key email and the specified columnOrdinal .
         /// </summary>
-        /// <param name="email">the email of the user that the board belong</param>
-        /// <param name="columnOrdinal">the column number of the board</param>
-        /// <returns>return Column with the spesified columnOrdinal in the board with the key email</returns>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <returns>return Column with the spesified columnOrdinal in the board with the key email.</returns>
         public Column GetColumn(string email, int columnOrdinal)
         {
             Board b = GetBoard(email);
@@ -78,12 +78,12 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
         }
 
         /// <summary>
-        /// limit the number of taks that the columns with columnOrdinal can hold
+        /// limit the number of taks that the columns with columnOrdinal can hold.
         /// </summary>
-        /// <param name="email">the email of the user that the board belong</param>
-        /// <param name="columnOrdinal">the column number of the board</param>
-        /// <param name="limit">The number of maximum task that the column will hold</param>
-        /// <exception cref="ArgumentException">theown when tring to set limit to s number less or equal to 0</exception>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <param name="limit">The number of maximum task that the column will hold.</param>
+        /// <exception cref="ArgumentException">theown when tring to set limit to s number less or equal to 0.</exception>
         public void LimitColumnTask(string email, int columnOrdinal, int limit)
         {
             Board b = GetBoard(email);
@@ -97,7 +97,15 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             else
                 throw new ArgumentException("limit must be a natural non zero number");
         }
-
+        /// <summary>
+        /// Advence a task from specified column to the next one.
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <param name="taskId">The task ID to advance.</param>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Trown if trying to advance from Done column or is the next column is full.
+        /// </exception>
         public void AdvanceTask(string email, int columnOrdinal,int taskId)
         {
             Board b = GetBoard(email);
@@ -122,7 +130,19 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
                 log.Debug("task " + taskId + " was advanced");
             }
         }
-
+        /// <summary>
+        /// Adds a new task to "Backlog" column.
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="title">Title of the task.</param>
+        /// <param name="description">Description of the new task.</param>
+        /// <param name="dueDate">Duedate of the new task.</param>
+        /// <returns>
+        /// return the new task that was created.
+        /// </returns>
+        /// <exception cref="Exception">
+        /// thrown is "Backlog" column is full.
+        /// </exception>
         public Task AddTask(string email, string title, string description, DateTime dueDate)
         {
             Column c = GetColumn(email, "Backlog");
@@ -140,7 +160,13 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             log.Debug("new task was added to Backlog Column");
             return newTask;            
         }
-
+        /// <summary>
+        /// update the title of the task with taskId 
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <param name="taskId">The task ID to update.</param>
+        /// <param name="newTitle">the new title to insert</param>
         public void UpdateTaskTitle(string email, int columnOrdinal, int taskId, string newTitle)
         {
             Task toUpdate = GetColumn(email, columnOrdinal).GetTask(taskId);
@@ -149,6 +175,13 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             log.Debug("Task title was updated");
         }
 
+        /// <summary>
+        /// update the description of the task with taskId 
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <param name="taskId">The task ID to update.</param>
+        /// <param name="newTitle">the new desctiption to insert</param>
         public void UpdateTaskDescription(string email, int columnOrdinal, int taskId, string newDescription)
         {
             Task toUpdate = GetColumn(email, columnOrdinal).GetTask(taskId);
@@ -157,6 +190,13 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             log.Debug("Task description was updated");
         }
 
+        /// <summary>
+        /// update the duedate of the task with taskId 
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
+        /// <param name="columnOrdinal">the column number of the board.</param>
+        /// <param name="taskId">The task ID to update.</param>
+        /// <param name="newTitle">the new duedate to insert</param>
         public void UpdateTaskDueDate(string email, int columnOrdinal, int taskId, DateTime newDueDate)
         {
             Task toUpdate = GetColumn(email, columnOrdinal).GetTask(taskId);
@@ -165,6 +205,10 @@ namespace IntroSE.Kanban.Backend.BusinessLayer.BoardPackage
             log.Debug("Task doudate was updated");
         }
 
+        /// <summary>
+        /// Adds a new board to the Boards dictionary
+        /// </summary>
+        /// <param name="email">the email of the user that the board belong.</param>
         public void AddNewBoard(string email)
         {
             _boards.Add(email, new Board(email));
