305030a3eab7152fa348106b10b58c3c789b3b88
paveltom <paveltom@post.bgu.ac.il>
2020-04-15 14:27:33 +0300

    routine: Program.cs added

---
+++ Backend/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroSE.Kanban.Backend
+{
+    class Program
+    {
+        public static void Main()
+        {
+            ServiceLayer.Service service = new ServiceLayer.Service();
+        }
+    }
+}
