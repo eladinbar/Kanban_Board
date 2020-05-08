622bd7aa8f28c1668a008d322f00d2fa751846ea
eladinbar <eladin@post.bgu.ac.il>
2020-05-08 17:55:07 +0300

    NUnit setup

--- Backend/Properties/AssemblyInfo.cs
+++ Backend/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: InternalsVisibleToAttribute("TestsLibrary")]
+[assembly: InternalsVisibleTo("KanbanBoardTest")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
