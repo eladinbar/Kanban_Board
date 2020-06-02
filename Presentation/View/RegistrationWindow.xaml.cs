77c3edcc8148f19d9c32b29e52d86d0d35f3c0d8
Edan Levy <edal@post.bgu.ac.il>
2020-06-02 17:38:45 +0300

    Rgistration window design

---
+++ Presentation/View/RegistrationWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Presentation.View
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class RegistrationWindow : Window
+    {
+        public RegistrationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ConfirmRegistration_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
