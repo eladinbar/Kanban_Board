995bf989fe8db29335ca946b455ebc8918444bea
Edan Levy <edal@post.bgu.ac.il>
2020-06-07 15:42:59 +0300

    more methods

---
+++ BackendTesting/TaskTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IntroSE.Kanban.Backend.ServiceLayer;
+using NUnit.Framework;
+
+namespace BackendTesting
+{
+    [TestClass]
+    public class TaskTests
+    {
+
+        public IService service = new Service();
+        public User LoggedUser;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            service.Register("1@mashu.com", "Aa1234", "1");
+            service.Register("2@mashu.com", "Aa1234", "2", "1@mashu.com");
+            LoggedUser = service.Login("1@mashu.com", "Aa1234").Value;
+        }
+
+
+        [Test]
+        public void AddTaskTest_ValidInput()
+        {
+            string expectedTitle = "my title";
+            string expectedDescription = "my Description";
+            string assigneeEmail = "1@mashu.com";
+
+            Response<Task> r1 = service.AddTask(LoggedUser.AssociatedBoard, expectedTitle, expectedDescription, new DateTime(2021,3,5));
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase(expectedTitle, r1.Value.Title, r1.ErrorMessage);
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase(expectedDescription, r1.Value.Description, r1.ErrorMessage);
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase(assigneeEmail, r1.Value.emailAssignee, r1.ErrorMessage);
+
+        }
+
+
+
+    }
+}
--- BackendTesting/UserTests.cs
+++ BackendTesting/UserTests.cs
@@ -10,21 +10,23 @@ namespace BackendTesting
     public class UserTests
     {
         IService service = new Service();
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            //service.DeleteData();
+            //string path = @"D:\UProject\IntroToSE\milestones-2-tpee\BackendTesting\bin\Debug\KanbanDB.db";
+            //FileInfo dBFile = new FileInfo(path);
+            //if (dBFile.Exists)
+            //{
+            //    dBFile.Delete();
+            //}
+
+            service = new Service();
+            service.Register("1@mashu.com", "Aa1234", "1");
+            service.Register("2@mashu.com", "Aa1234", "2", "1@mashu.com");
+        }
         
-
-        [Test]
-        public void RegisterTest()
-        {
-            Response r1 = service.Register("1@mashu.com", "Aa1234", "1");
-            NUnit.Framework.Assert.IsFalse(r1.ErrorOccured);
-        }
-
-        public void CheckAssosiatedBoard()
-        {
-            User u1;
-            u1 = service.Login("1@mashu.com", "Aa1234").Value;
-            NUnit.Framework.Assert.True(u1.Asso)
-        }
 
         [Test]
         public void RegisterTest_AlreadyRegisterd()
@@ -34,31 +36,44 @@ namespace BackendTesting
         }
 
         [Test]
-        public void RegisterTest_JoinBoard()
-        {
-            Response r1 = service.Register("2@mashu.com", "Aa1234", "2","1@mashu.com");
-            NUnit.Framework.Assert.IsFalse(r1.ErrorOccured);
-        }
-
-
-        [Test]
         public void RegisterTest_JoinBoard_NotExisting()
         {
-            Response r1 = service.Register("2@mashu.com", "Aa1234", "2", "4@mashu.com");
+            Response r1 = service.Register("3@mashu.com", "Aa1234", "2", "4@mashu.com");
             NUnit.Framework.Assert.IsTrue(r1.ErrorOccured);
         }
 
-
-        [OneTimeTearDown]
-        public void CleanUp()
+        [TestCase("1@mashu.com")]
+        [TestCase("2@mashu.com")]
+        public void LoginTest_ExcistingUser(string userTochack)
         {
-            service.DeleteData();
-            string path = @"D:\UProject\IntroToSE\milestones-2-tpee\BackendTesting\bin\Debug\KanbanDB.db";
-            FileInfo dBFile = new FileInfo(path);
-            if (dBFile.Exists)
-            {
-                dBFile.Delete();
-            }
+            string expectedUser = userTochack;
+           
+            Response<User> r1 = service.Login(userTochack, "Aa1234");
+            NUnit.Framework.StringAssert.AreEqualIgnoringCase(expectedUser, r1.Value.Email, r1.ErrorMessage);
+            service.Logout(userTochack);
+
         }
+
+
+        
+        //[Test]
+        //public void CheckAssosiatedBoard(string user)
+        //{
+        //    User u1 = service.Login("1@mashu.com", "Aa1234").Value;
+        //    NUnit.Framework.Assert.True(u1.AssociatedBoard.Equals("1@mashu.com"));
+        //    service.Logout(user);
+        //}
+
+        //[Test]
+        //public void CheckAssosiatedBoard2(string user)
+        //{
+        //    User u1;
+        //    u1 = service.Login("2@mashu.com", "Aa1234").Value;
+        //    NUnit.Framework.Assert.True(u1.AssociatedBoard.Equals("1@mashu.com"));
+        //    service.Logout(user);
+        //}
+
+
+       
     }
 }
