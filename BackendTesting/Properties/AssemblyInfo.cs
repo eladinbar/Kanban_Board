36060e232446bbc6a984c1b32785fffcab00393a
Edan Levy <edal@post.bgu.ac.il>
2020-06-07 11:46:52 +0300

    building unit testing

---
+++ BackendTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BackendTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BackendTesting")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("953f88e0-64da-406b-9e74-b3268d4f7b12")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ BackendTesting/UserTests.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using IntroSE.Kanban.Backend.ServiceLayer;
+using System.IO;
+
+namespace BackendTesting
+{
+    [TestClass]
+    public class UserTests
+    {
+        IService service = new Service();
+        
+
+        [Test]
+        public void RegisterTest()
+        {
+            Response r1 = service.Register("1@mashu.com", "Aa1234", "1");
+            NUnit.Framework.Assert.IsFalse(r1.ErrorOccured);
+        }
+
+        public void CheckAssosiatedBoard()
+        {
+            User u1;
+            u1 = service.Login("1@mashu.com", "Aa1234").Value;
+            NUnit.Framework.Assert.True(u1.Asso)
+        }
+
+        [Test]
+        public void RegisterTest_AlreadyRegisterd()
+        {
+            Response r1 = service.Register("1@mashu.com", "Aa1234", "2");
+            NUnit.Framework.Assert.IsTrue(r1.ErrorOccured);
+        }
+
+        [Test]
+        public void RegisterTest_JoinBoard()
+        {
+            Response r1 = service.Register("2@mashu.com", "Aa1234", "2","1@mashu.com");
+            NUnit.Framework.Assert.IsFalse(r1.ErrorOccured);
+        }
+
+
+        [Test]
+        public void RegisterTest_JoinBoard_NotExisting()
+        {
+            Response r1 = service.Register("2@mashu.com", "Aa1234", "2", "4@mashu.com");
+            NUnit.Framework.Assert.IsTrue(r1.ErrorOccured);
+        }
+
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            service.DeleteData();
+            string path = @"D:\UProject\IntroToSE\milestones-2-tpee\BackendTesting\bin\Debug\KanbanDB.db";
+            FileInfo dBFile = new FileInfo(path);
+            if (dBFile.Exists)
+            {
+                dBFile.Delete();
+            }
+        }
+    }
+}
