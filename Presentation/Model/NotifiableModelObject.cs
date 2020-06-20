641e45885d57dbf305bd63929591c1ba53befaab
Elad Inbar <eladin@post.bgu.ac.il>
2020-06-20 22:04:06 +0300

    Adjusted board documentation

--- Presentation/Model/NotifiableModelObject.cs
+++ Presentation/Model/NotifiableModelObject.cs
@@ -5,6 +5,11 @@ namespace Presentation.Model
     public class NotifiableModelObject : NotifiableObject
     {
         public BackendController Controller { get; private set; }
+
+        /// <summary>
+        /// NotifiableModelObject constructor. Initializes the controller the model object uses to interact with the backend.
+        /// </summary>
+        /// <param name="controller"></param>
         protected NotifiableModelObject(BackendController controller)
         {
             Controller = controller;
